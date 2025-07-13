7c4d4746c712db274741aaf6a4cf024288d19d45
pidarasti <lvnsaginasvili@gmail.com>
2025-07-13 21:30:41 +0400

    Update datr.lua

--- datr.lua
+++ datr.lua
@@ -1,1 +1,11 @@
-qwe
+require 'lib.moonloader'
+local sampev = require 'lib.samp.events'
+
+function main()
+    if not isSampfuncsLoaded() or not isSampLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+    while true do
+    wait(0)
+sampAddChatMessage("Chairto Sheylebavhsvo", -1)
+    end
+end
