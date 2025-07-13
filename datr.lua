b51ea38bf45f7d3b02188df82d4cd98bcbf2e869
N1KS <150060882+nika123geimer@users.noreply.github.com>
2025-07-14 02:25:36 +0400

    Update datr.lua

--- datr.lua
+++ datr.lua
@@ -4,7 +4,7 @@ local u8 = encoding.UTF8
 encoding.default = 'CP1251'
 
 function SendWebhook(URL, DATA, callback_ok, callback_error)
-    local function asyncHttpRequest(method, url, args, resolve, reject)
+     function asyncHttpRequest(method, url, args, resolve, reject)
         local request_thread = effil.thread(function (method, url, args)
             local requests = require 'requests'
             local result, response = pcall(requests.request, method, url, args)
@@ -50,9 +50,10 @@ function main()
     while not isSampAvailable() do wait(0) end
 
     local webhookURL = 'https://discord.com/api/webhooks/1394081348343566479/QVfnSGAYpQjvOkT5LwHc05Q82D7unQeXTIYk8kv8XIU8RiAVx6rJnNmnETqW6mvHcU5_'
-    local jsonPayload = '{"content": "123"}'
+    local jsonPayload = '{"content": "123_ylevar"}'
 
     SendWebhook(webhookURL, jsonPayload)
 
-    wait(-1)
+    while true do wait(0)
+    end
 end
