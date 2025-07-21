99b2af02271bb7a1d4e5cef96fcc1317148bd133
N1KS <150060882+nika123geimer@users.noreply.github.com>
2025-07-21 15:51:38 +0400

    Update datr.lua

--- datr.lua
+++ datr.lua
@@ -1,1 +1,33 @@
-print("")
+
+
+
+sampev = require "lib.samp.events"
+
+qwerty = false 
+
+function sampev.onPlayerChatBubble(id, col, dist, dur, msg)
+    if msg:find("habsburgs") then 
+
+        tests()
+
+    end
+
+end
+
+function tests()
+    lua_thread.create(function()
+        qwerty = true 
+        wait(1000)
+        sampSendChat(":D")
+        wait(2000)
+        qwerty = false 
+    end)
+end
+
+function sampev.onServerMessage(color, text)
+
+    if qwerty then 
+        return false  
+    end 
+
+end
